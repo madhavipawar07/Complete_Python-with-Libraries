23885ee98bf25296ea4538d23ee6946a5bab2064
madhavi <madhavippawar2004@gmail.com>
2026-04-26 18:47:13 +0530

    Complete Python with Libraries

---
+++ function_caching.py
@@ -0,0 +1,25 @@
+from functools import lru_cache
+import time
+
+@lru_cache(maxsize=None)
+def fx(n):
+    time.sleep(5)
+    return n * 5
+
+print(fx(5))
+print("Done for 5")
+
+print(fx(4))
+print("Done for 4")
+
+print(fx(6))
+print("Done for 6")
+
+print(fx(5))
+print("Done for 5")
+
+print(fx(4))
+print("Done for 4")
+
+print(fx(6))
+print("Done for 6")
