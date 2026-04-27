13010d691c8f2e94e2772c7774ef02f94e6413d2
madhavi <madhavippawar2004@gmail.com>
2026-04-27 08:49:17 +0530

    Complete Python with Libraries

--- AsyncIO-lib.py
+++ AsyncIO-lib.py
@@ -6,3 +6,16 @@ async def task():
 
 asyncio.run(task())
 print("Done")
+
+async def download(name,time):
+    print(f"Downloading{name}..")
+    await asyncio.sleep(time)
+    print(f"{name} downloaded")
+
+async def main():
+    await asyncio.gather(
+        download("https://www.freepik.com/free-photos-vectors/imag",2),
+        download("https://www.freepik.com/free-photos-vectors/landscape",3),
+        download("https://www.bucketlistly.blog/posts/best-free-travel-images",1)
+    )
+asyncio.run(main())
