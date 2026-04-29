bbddd74d98006599d37caa6e2c77e62ecdc5585e
madhavi <madhavippawar2004@gmail.com>
2026-04-29 12:16:48 +0530

    Complete Python with Libraries

--- Matplotlib-Library/PyPlotSubModule.py
+++ Matplotlib-Library/PyPlotSubModule.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = np.array([0, 6])
+# xpoints = np.array([0, 6])
 ypoints = np.array([0, 250])
 
 plt.plot(xpoints, ypoints)
@@ -15,3 +15,17 @@ ypoints = np.array([3, 10])
 
 plt.plot(xpoints, ypoints)
 plt.show()
+
+
+#Matplotlib Markers
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, marker = 'o')
+plt.show()
+
+#Matplotlib Line
+ypoints = np.array([3, 8, 1, 10])
+
+plt.plot(ypoints, linestyle = 'dotted')
+plt.show()
+
