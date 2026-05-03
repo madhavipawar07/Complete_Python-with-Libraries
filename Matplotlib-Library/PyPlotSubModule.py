a0dc477c49c1e863475cb4fa265ac124bd76f23c
madhavi <madhavippawar2004@gmail.com>
2026-05-03 21:47:25 +0530

    Complete Python with Libraries

--- Matplotlib-Library/PyPlotSubModule.py
+++ Matplotlib-Library/PyPlotSubModule.py
@@ -29,7 +29,7 @@ ypoints = np.array([3, 8, 1, 10])
 plt.plot(ypoints, linestyle = 'dotted')
 plt.show()
 
-#Labels for a Plot
+#Labels for a Plot 
 x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
 y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 
