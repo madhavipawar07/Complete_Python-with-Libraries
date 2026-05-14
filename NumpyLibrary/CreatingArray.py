6e5702bad74db2c223b607765a672ff3fd7035bb
madhavi <madhavippawar2004@gmail.com>
2026-05-14 23:22:13 +0530

    Complete Python with Libraries

--- NumpyLibrary/CreatingArray.py
+++ NumpyLibrary/CreatingArray.py
@@ -1,12 +1,13 @@
 import numpy as np
-
 arr = np.array([1, 2, 3, 4, 5])
-
 print(arr)
-
 print(type(arr))
 
 #Array Indexing
-arr = np.array([1, 2, 3, 4])
 
+arr = np.array([1, 2, 3, 4])
 print(arr[0])
+
+arr = np.array([1, 2, 3, 4])
+print(arr[2] + arr[3])
+
