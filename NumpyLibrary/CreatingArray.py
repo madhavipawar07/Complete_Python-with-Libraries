326211e1ef4005311fd5452c4773cc8f4f51cc21
madhavi <madhavippawar2004@gmail.com>
2026-05-16 07:01:55 +0530

    Complete Python with Libraries

--- NumpyLibrary/CreatingArray.py
+++ NumpyLibrary/CreatingArray.py
@@ -12,5 +12,11 @@ arr = np.array([1, 2, 3, 4])
 print(arr[2] + arr[3])
 
 #2D Array Indexing
+
 arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
 print('2nd element on 1st row: ', arr[0, 1])
+
+#Array Slicing
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[1:5])
