45fb9fd38f4f906b6471e73b83d90bceb7f84ef5
madhavi <madhavippawar2004@gmail.com>
2026-05-28 04:10:08 +0530

    Complete Python with Libraries

--- NumpyLibrary/ShapingArray.py
+++ NumpyLibrary/ShapingArray.py
@@ -8,3 +8,9 @@ arr = np.array([1, 2, 3, 4], ndmin=5)
 
 print(arr)
 print('shape of array :', arr.shape)
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+newarr = arr.reshape(4, 3)
+
+print(newarr)
